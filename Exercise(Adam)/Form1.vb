6c13677a0116bb88cb85912a82661218c0d7463f
Adam Shah <adamshahmizi487@gmail.com>
2023-09-18 09:36:21 +0800

    first commit

---
+++ Exercise(Adam)/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        About.Show()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+End Class
